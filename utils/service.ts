f79cb930a73cc1a2e7c6ccfd858dd08e382fd6f9
imkiva <imkiva@islovely.icu>
2025-11-05 18:07:42 +0800

    misc: use `no-store` for video index cache
    - https://nextjs.org/docs/app/getting-started/fetching-data

--- utils/service.ts
+++ utils/service.ts
@@ -27,7 +27,7 @@ async function fetchWithDefaults(
       ...options.headers,
     },
     body: options.body ? JSON.stringify(options.body) : null,
-    cache: options.cache || "no-cache",
+    cache: options.cache || "no-store",
   };
 
   try {
